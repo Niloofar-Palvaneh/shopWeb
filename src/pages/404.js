c8d4ef069439a7d1b4d86924b0f69306ef9b13e5
niloofar-palvanehcls <niloofarpalvaneh@gmail.com>
2023-10-08 21:03:16 +0330

    ..

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ export default function Error() {
         <>
             <div className="flex items-center justify-center flex-col gap-4 mt-12 text-3xl text-red-500 font-bold">
                 this url not found
-                <Image src={"/imgs/404.gif"} width={100} height={100} className="w-1/2" />
+                <Image src={"/imgs/404.gif"} width={100} height={100} className="w-1/2 sm:w-full" />
             </div>
         </>
     )
